72b38ebff8865f195e484cbe4767c097fab32658
mbskot2 <mbskot@gmail.com>
2014-01-07 09:24:47 +0100

    Zmazanie nepotrebnej funkcie z BooleanParamEntity.

--- app/CertificatesModule/Models/Param/BooleanParamEntity.php
+++ app/CertificatesModule/Models/Param/BooleanParamEntity.php
@@ -2,7 +2,6 @@
 namespace CertificatesModule\Models\Param;
 
 use CertificatesModule\Models\Certificate\CertificateEntity,
-	CertificatesModule\Models\ParamType\ParamType,
 	CertificatesModule\Models\ParamType\ParamTypeEntity,
 	Doctrine\ORM\Mapping as ORM;
 
@@ -46,29 +45,7 @@ class BooleanParamEntity extends ParamEntity
 	{
 		$this->value = $value;
 	}
-	
-	/**
-	 * @param int $paramTypeId
-	 * @return mixed
-	 */
-	public function convertValueTo($paramTypeId)
-	{
-		switch ($paramTypeId)
-		{
-			case ParamType::INTEGER:
-				return (int) $this->value;
-			case ParamType::DOUBLE:
-				return (double) $this->value;
-			case ParamType::STRING:
-			case ParamType::TEXT:
-				return (string) $this->value;
-		}
-		
-		throw new \Nette\InvalidArgumentException(
-			sprintf('Can not convert boolean to %s.', ParamType::getLabel($paramTypeId))
-		);
-	}
-	
+
 	/**
 	 * @return string
 	 */
